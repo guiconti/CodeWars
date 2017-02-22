ac6b0cd938444705f72d58e465d8fb29a2a496b6
GuiMac <guibasconti@gmail.com>
2017-02-21 23:37:00 -0300

    Better Human Readable

--- 5Kyu/humanReadable.js
+++ 5Kyu/humanReadable.js
@@ -1,8 +1,8 @@
 function humanReadable(seconds) {
 
   var hours = Math.floor(seconds/3600);
-  var minutes = Math.floor((seconds-hours*3600)/60);
-  var seconds = Math.floor((seconds-hours*3600)-(minutes*60));
+  var minutes = Math.floor(seconds/60 % 60);
+  var seconds = Math.floor(seconds%60);
   
   return ((hours/10)>=1?"":"0") + hours + ":" + ((minutes/10)>=1?"":"0") + minutes + ":" + ((seconds/10)>=1?"":"0") + seconds; 
 
