cee842430667691af9d30aaf4bab8f6a987ffa01
GuiMac <guibasconti@gmail.com>
2017-02-21 23:17:41 -0300

    Is Valid Walk

---
+++ 6Kyu/isValidWalk.js
@@ -0,0 +1,7 @@
+function isValidWalk(walk) {
+
+  return walk.length===10&&
+  ((walk.toString().match(/n/g)?walk.toString().match(/n/g).length:0)===(walk.toString().match(/s/g)?walk.toString().match(/s/g).length:0))&&
+  ((walk.toString().match(/w/g)?walk.toString().match(/w/g).length:0)===(walk.toString().match(/e/g)?walk.toString().match(/e/g).length:0));
+
+}
